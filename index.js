1afb5862f29a76242bdd2575da8402e8562c90ba
allanyu <googleyufei@qq.com>
2016-07-08 10:20:29 +0800

    bugfix

--- index.js
+++ index.js
@@ -6,64 +6,62 @@ exports.name = 'upload';
 exports.desc = 'upload file or the files of destination path.';
 
 exports.register = function (commander, settings) {
-    commander.option('-p, --path <path>', 'set path')
-             .option('-f, --file <file>', 'set file')
-             .action(function (template) {
-                var args = [].slice.call(arguments);
-                var options = args.pop();
+    commander.option('-p, --path <path>', 'waiting upload dir')
+        .option('-f, --file <file>', 'waiting upload file')
+        .option('--verbose', 'enable verbose mode')
+        .action(function (template) {
+            var args = [].slice.call(arguments);
+            var options = args.pop();
 
-                var to = args[0],
-                    distPath = options.path,
-                    filePath = options.file,
-                    receiver = "http://wapstatic.kf0309.3g.qq.com/receiver/receiver2.php",
-                    formData;
+            var to = args[0],
+                distPath = options.path,
+                filePath = options.file,
+                receiver = "http://wapstatic.kf0309.3g.qq.com/receiver/receiver2.php",
+                formData;
 
-                if(!filePath && !distPath){
-                    fis.log.error("file or path is required!");
-                    return process.exit(0);
-                }
+            if (!filePath && !distPath) {
+                fis.log.error("file or path is required!");
+                return process.exit(0);
+            }
 
-                if(!to || typeof to === "object"){
-                    to = '/data/wapstatic/tmp';
-                }
-
-                if(distPath){
-                    distPath = path.resolve(distPath);
-                    fis.log.info("Start upload path->'"+ distPath +"' files");
-                    explorer(distPath, function(file){
-                        var relativePath = path.relative(distPath, file),
-                            basename = path.basename(file); //TODO 过滤文件
-                        formData = {
-                            to: to + "/" + relativePath,
-                            file: fs.createReadStream(file)
-                        };
-                        request.post({url:receiver, formData: formData}, function optionalCallback(err, httpResponse, body) {
-                            if (err) {
-                                return fis.log.error('upload failed:', relativePath + "\n Error:" +  err);
-                            }
-                            fis.log.info("Upload "+ relativePath +" successful!");
-                        });
-                    });
-                }else{
+            if (!to || typeof to === "object") {
+                to = '/data/wapstatic/tmp';
+            }
+            fis.log.info("options path: ", options.path, ", file: ", options.file);
+            if (distPath) {
+                distPath = path.resolve(distPath);
+                fis.log.info("Start upload path->'" + distPath + "' files");
+                explorer(distPath, function (file) {
+                    var relativePath = path.relative(distPath, file),
+                        basename = path.basename(file); //TODO 过滤文件
                     formData = {
-                        to: to,
-                        file: fs.createReadStream(path.resolve(filePath))
+                        to: to + "/" + relativePath,
+                        file: fs.createReadStream(file)
                     };
-                    var ext = path.extname(filePath),
-                        basename = path.basename(filePath);
-                    if(ext === ".zip") {
-                        formData.type = ext.substr(1);
-                    }else{
-                        formData.to = formData.to + "/" + basename
-                    }
-                    request.post({url:receiver, formData: formData}, function optionalCallback(err, httpResponse, body) {
+                    request.post({ url: receiver, formData: formData }, function optionalCallback(err, httpResponse, body) {
                         if (err) {
-                            return fis.log.error('upload failed:', filePath + "\n Error:" +  err);
+                            return fis.log.error('upload failed:', relativePath + "\n Error:" + err);
                         }
-                        fis.log.info("Upload successful!");
+                        fis.log.info("Upload " + relativePath + " successful!");
                     });
-                }
-            });
+                });
+            } else {
+                formData = {
+                    to: to,
+                    file: fs.createReadStream(path.resolve(filePath))
+                };
+                var ext = path.extname(filePath),
+                    basename = path.basename(filePath);
+                formData.to = formData.to + "/" + basename;
+                fis.log.info("start upload file ", filePath, ', to dest: ', formData.to);
+                request.post({ url: receiver, formData: formData }, function optionalCallback(err, httpResponse, body) {
+                    if (err) {
+                        return fis.log.error('upload failed:', filePath + "\n Error:" + err);
+                    }
+                    fis.log.info("Upload successful!");
+                });
+            }
+        });
 };
 
 /**
@@ -72,25 +70,25 @@ exports.register = function (commander, settings) {
  * @return 
  */
 function explorer(dpath, callback) {
-    fs.readdir(dpath, function(err, files) {
+    fs.readdir(dpath, function (err, files) {
         if (err) {
             console.log("error: \n" + err);
             return;
         }
 
-        files.forEach(function(file) {
-            fs.stat(dpath + "\\" + file + "",function(err,stat){
-                if(err){
+        files.forEach(function (file) {
+            fs.stat(dpath + "\\" + file + "", function (err, stat) {
+                if (err) {
                     console.log(err);
                     return;
                 }
 
-                if(stat.isDirectory()){
-                    explorer(dpath+"\\"+file, callback);
-                }else{
-                    callback && callback(dpath+"\\"+file);
+                if (stat.isDirectory()) {
+                    explorer(dpath + "\\" + file, callback);
+                } else {
+                    callback && callback(dpath + "\\" + file);
                 }
             });
         });
-    });    
+    });
 }
